f03811b285f16f42b94d06802486e5e2751283db
Justus Post <JPost@lcs.on.ca>
2020-05-01 13:38:20 +0200

    I have created four ellipses drawing a cross

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -20,6 +20,11 @@ class Sketch : NSObject {
         // Set starting position for all the lines
         offset = 250
 
+        //rotate the canvas 45 degrees
+//        canvas.rotate(by: 45)
+        
+        //turn of borders
+        canvas.drawShapesWithBorders = false
     }
     
     // This function runs repeatedly, forever, to create the animated effect
@@ -29,16 +34,20 @@ class Sketch : NSObject {
         offset += 1
         
         // Draw a red ellipse in the middle of the canvas going up and right
+        canvas.fillColor = .red
         canvas.drawEllipse(at: Point(x: offset, y: 250), width: 50, height: 50)
         
         // Draw a purple ellipse in the middle of the canvas going down and right
-        canvas.drawEllipse(at: Point(x: offset, y: 250), width: 50, height: 50)
+        canvas.fillColor = .purple
+        canvas.drawEllipse(at: Point(x: 250, y: canvas.width - offset), width: 50, height: 50)
         
         // Draw a green ellipse in the middle of the canvas going up and left
-        canvas.drawEllipse(at: Point(x: offset, y: 250), width: 50, height: 50)
+        canvas.fillColor = .green
+        canvas.drawEllipse(at: Point(x: 250, y: offset), width: 50, height: 50)
         
         // Draw a yellow ellipse in the middle of the canvas going down and left
-        canvas.drawEllipse(at: Point(x: offset, y: 250), width: 50, height: 50)
+        canvas.fillColor = .yellow
+        canvas.drawEllipse(at: Point(x: canvas.width - offset, y: 250), width: 50, height: 50)
         
         
     }
